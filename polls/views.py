af69c9aa98a9e5d97134bfe109fff1796adcedc4
tobuya <thomasobuya95@gmail.com>
2024-01-12 21:39:57 +0300

    Render the index template on the index method

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# from .models import Question
+from . import models
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world! You are in the polls index page.")
+    latest_question_list = models.Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
