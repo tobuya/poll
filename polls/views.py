f67f0683f961b1da7749837952314f9584e0c5a3
tobuya <thomasobuya95@gmail.com>
2024-01-12 23:05:52 +0300

    Render detail page or raise Http404 error when page doesn't exist

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.conf import empty
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+# There’s also a get_list_or_404() function, which works just as get_object_or_404()
+# except using filter() instead of get(). It raises Http404 if the list is empty
 
 # from .models import Question
 from . import models
@@ -11,7 +15,12 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    # try:
+    #     question = models.Question.objects.get(pk = question_id)
+    # except models.Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(models.Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
