8e64f405db630882ebd85fd060d7a22d388f4eed
tobuya <thomasobuya95@gmail.com>
2024-01-13 17:58:01 +0300

    Introduce new views based on Django's generic views

--- polls/views.py
+++ polls/views.py
@@ -1,37 +1,37 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
 
 # There’s also a get_list_or_404() function, which works just as get_object_or_404()
 # except using filter() instead of get(). It raises Http404 if the list is empty
 
 # from .models import Question
-from . import models
+from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    latest_question_list = models.Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    # try:
-    #     question = models.Question.objects.get(pk = question_id)
-    # except models.Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    question = get_object_or_404(models.Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(models.Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
-    question = get_object_or_404(models.Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, models.Choice.DoesNotExist):
+    except (KeyError, Choice.DoesNotExist):
         return render(
             request,
             "polls/detail.html",
