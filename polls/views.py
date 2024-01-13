819795fdf0c35d33e98e74b21274e264e0544db7
tobuya <thomasobuya95@gmail.com>
2024-01-13 16:18:38 +0300

    Modify the votes method to process data from the form

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.conf import empty
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
 
 # There’s also a get_list_or_404() function, which works just as get_object_or_404()
 # except using filter() instead of get(). It raises Http404 if the list is empty
@@ -23,8 +23,22 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    response = "You are looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(models.Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(models.Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, models.Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+             {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+            )
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
