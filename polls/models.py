b5b4eb42769656a648877173e440a219f9ed505e
tobuya <thomasobuya95@gmail.com>
2024-01-15 22:14:27 +0300

    Fix the was_published_recently method to only return True if the date is also in the past

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+'''Importing necessary modules and classes'''
 import datetime
 
 from django.db import models
@@ -5,17 +6,21 @@ from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
+    """Class representing a question."""
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+        """Function getting recent dates."""
+        now = timezone.now()
+        return now - datetime.timedelta(days = 1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
 
 
 class Choice(models.Model):
+    """Class representing choice."""
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
