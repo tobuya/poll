ff85af106a0ffd3d0c0ec35121ffe2293e584b64
tobuya <thomasobuya95@gmail.com>
2024-01-16 12:10:39 +0300

    Fetch last 5 published questions excluding those scheduled for future publication

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
+'''Importing necessary modules and classes'''
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 # There’s also a get_list_or_404() function, which works just as get_object_or_404()
 # except using filter() instead of get(). It raises Http404 if the list is empty
@@ -12,22 +14,26 @@ from .models import Question, Choice
 
 # Create your views here.
 class IndexView(generic.ListView):
+    """Class utilizing ListView generic view."""
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
+    """Class utilizing DetailView generic view."""
     model = Question
     template_name = "polls/detail.html"
 
 class ResultsView(generic.DetailView):
+    """Class utilizing DetailView generic view."""
     model = Question
     template_name = "polls/results.html"
 
 def vote(request, question_id):
+    """Function for voting a question."""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
